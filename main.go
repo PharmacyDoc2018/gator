9da8565e6b3911a527cbe0bb9df4070513ffcab6
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-06-20 21:05:11 -0400

    fixed write() to properly write to .gatorconfig.json

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/PharmacyDoc2018/gator/internal/config"
 )
 
-func main() {
-	gatorConfig, err := config.Read()
+func printConfigFile() {
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println(err)
 	}
+	filePath := homeDir + "/.gatorconfig.json"
+	configFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	contents := make([]byte, 64)
+	configFile.Read(contents)
+	configFile.Close()
+	fmt.Println(string(contents))
 
-	gatorConfig.SetUser("Joseph")
+}
 
-	fmt.Println(gatorConfig.DbURL)
-	fmt.Println(gatorConfig.CurrentUserName)
+func main() {
+	printConfigFile()
+
+	fmt.Printf("\n")
+	configStruct, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+	}
+	configStruct.SetUser("Jessica")
+	fmt.Println(configStruct)
+	printConfigFile()
+
+	fmt.Printf("\n")
+	configStruct.SetUser("Joseph")
+	fmt.Println(configStruct)
+	printConfigFile()
+
 }
