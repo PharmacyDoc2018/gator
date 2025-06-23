02634f1d5009cd8fb68fb1ac201effdd755756f6
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-06-23 19:40:35 -0400

    finished handlerRegister() that registers a new user to the database. modified handlerLogin() to check user table before setting a user.

--- functions.go
+++ functions.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/PharmacyDoc2018/gator/internal/config"
 	"github.com/PharmacyDoc2018/gator/internal/database"
@@ -58,7 +59,13 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("argumment number error. expected 1 argument. receved %d", argNum)
 	}
 
-	err := s.config.SetUser(cmd.arguments[0])
+	loginName := cmd.arguments[0]
+	_, err := s.db.GetUser(context.Background(), loginName)
+	if err != nil {
+		return fmt.Errorf("error: user not found")
+	}
+
+	err = s.config.SetUser(loginName)
 	if err != nil {
 		return err
 	}
@@ -73,11 +80,27 @@ func handlerRegister(s *state, cmd command) error {
 		return fmt.Errorf("argumment number error. expected 1 argument. receved %d", argNum)
 	}
 
-	var params database.CreateUserParams
-		params.ID = uuid.New(),
-		params.CreatedAt = time.Now(),
-		params.UpdatedAt = time.Now(),
-		params.Name = cmd.arguments[0]
-	
-	s.db.CreateUser(context.Background(),params)
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.arguments[0],
+	}
+
+	newUser, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(newUser.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(newUser.Name, "was created")
+	fmt.Println("ID:", newUser.ID)
+	fmt.Println("CreatedAt:", newUser.CreatedAt)
+	fmt.Println("UpdatedAt:", newUser.UpdatedAt)
+	fmt.Println("Name:", newUser.Name)
+	return nil
 }
